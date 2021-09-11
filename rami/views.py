9d24214cde465e99b14895bb2241681318f6f292
Rami <rami.mahdoui@clevertech-france.fr>
2021-09-11 00:16:45 -0800

    first commit

--- rami/views.py
+++ rami/views.py
@@ -4,21 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
+
 
 def nomPage(request):
     return HttpResponse('<h1> Ahla b si Iheb</h1>')
 
 class ListUsers(APIView):
-    """
-    View to list all users in the system.
-
-    * Requires token authentication.
-    * Only admin users are able to access this view.
-    """
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
-
+    
     def get(self, request, format=None):
         """
         Return a list of all users.
