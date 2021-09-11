f8f034879fcae0d6b5cf2c31d51a345de23653a3
Rami <rami.mahdoui@clevertech-france.fr>
2021-09-11 00:22:40 -0800

    first commit

--- rami/urls.py
+++ rami/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.nomPage,name='index')
-     path('api', views.ListUsers.as_view(),name='api')
+    path('', views.nomPage,name='index'),
+    path('api', views.ListUsers.as_view(),name='api')
 ]
